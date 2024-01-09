5c2bfa0a8c38f90617715fb29fdf21126ec1811e
wwang71119 <84254276+wwang71119@users.noreply.github.com>
2024-01-09 10:52:40 -0800

    add mute_process, use bindgen to avoid issue we will face late if header file changes. fmt and lint fix. (#3)

--- build.rs
+++ build.rs
@@ -1,4 +1,16 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     println!("cargo:rustc-link-lib=dylib=EndpointSecurity");
     println!("cargo:rustc-link-lib=dylib=bsm");
+    let bindings = bindgen::Builder::default()
+        .header("wrapper.h")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable.to generate bindings");
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("could not write bindings!");
 }
